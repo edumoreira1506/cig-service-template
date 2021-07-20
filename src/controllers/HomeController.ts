03366fe6ec9a9096c2c139b4f4583b173c425dff
Eduardo Moreira <contato@eduardoem.com.br>
2021-07-19 21:52:12 -0300

    Setup i18n

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from 'express'
 
+import i18n from '@Configs/i18n'
+
 class HomeController {
   constructor() {
     this.index = this.index.bind(this)
   }
 
   async index(_: Request, res: Response): Promise<Response> {
-    return res.send({ message: 'Online api!' })
+    return res.send({ message: i18n.__('online-api') })
   }
 }
 
