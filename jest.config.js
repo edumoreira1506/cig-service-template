e00134037150750ce387c5dde1eab6db7453d7e4
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-09 10:58:50 -0300

    Bump dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const { compilerOptions } = require('./tsconfig.json')
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { pathsToModuleNameMapper } = require('ts-jest')
 
 module.exports = {
   // All imported modules in your tests should be mocked automatically
